57294ab4be8eea718ce032e89d27c24b6864cf8b
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2025-01-21 09:43:48 +0600

    fixing some bugs

--- app/Http/Controllers/Frontend/WishlistController.php
+++ app/Http/Controllers/Frontend/WishlistController.php
@@ -1,17 +1,14 @@
 <?php
-
 namespace App\Http\Controllers\Frontend;
 
-use App\Models\Wishlist;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
 
 class WishlistController extends Controller
 {
-    public function index(){
-        $wishlists=Wishlist::with('product')->where('user_id',Auth::user()->id)->paginate(8);
-        return $wishlists;
-        return view('frontend.pages.dashboard.wishlist',compact('wishlists'));
-    }
+    // public function index(){
+    //     $wishlists=Wishlist::with('product')->where('user_id',Auth::user()->id)->paginate(8);
+    //     return $wishlists;
+    //     return view('frontend.pages.dashboard.wishlist',compact('wishlists'));
+    // }
+
 }
